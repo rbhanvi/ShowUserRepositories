705c90f255dbca44635713469cd64dcc98827254
Bhanvi <rbhanvi053@gmail.com>
2018-07-24 23:52:45 +0200

    Changes done for error handling

--- src/app/components/userrepos/userrepos.component.ts
+++ src/app/components/userrepos/userrepos.component.ts
@@ -9,17 +9,25 @@ import { RepoService } from '../../services/repo.service';
 export class UserreposComponent implements OnInit {
   repos: any[];
   username:string;
+  public errorMsg;
 
   constructor(private reposervice: RepoService) { 
 
   }
 
   findProfile(){
+    if(this.username!=""){
     this.reposervice.updateProfile(this.username);
     this.reposervice.getProfileRepos().subscribe(repos => {
-      console.log(repos);
+      this.errorMsg = "";
       this.repos = repos;
-    })    
+    }, error => { this.errorMsg = error
+      this.repos = "";
+    }
+    )}
+    else{
+    this.repos="";
+    }
   }
 
   ngOnInit() {
--- src/app/services/repo.service.ts
+++ src/app/services/repo.service.ts
@@ -1,24 +1,33 @@
 import { Injectable } from '@angular/core';
 import {Http, Headers } from '@angular/http';
 import { map } from 'rxjs/operators';
-
-
+import {HttpErrorResponse} from '@angular/common/http';
+import { catchError } from 'rxjs/operators/catchError';
+import { throwError } from 'rxjs/operators/throwError';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RepoService {
 
-	private username:string;
-	private clientid = 'a76b0076c2942aa5a924';
-	private clientsecret = 'aa16161bc385f9800c0de0b200385bc465f922f6';
+  private username:string;
+  private clientid = 'a76b0076c2942aa5a924';
+  private clientsecret = 'aa16161bc385f9800c0de0b200385bc465f922f6';
   constructor(private http:Http) { 
-  	console.log("Service is now ready!!");
-  	this.username = 'rbhanvi';
+    console.log("Service is now ready!!");
+    this.username = 'rbhanvi';
   }
   getProfileRepos(){
     return this.http.get("https://api.github.com/users/" + this.username + "/repos?client_id=" + this.clientid + "&client_secret=" + this.clientsecret)
-    .pipe(map((res: any) => res.json()));
+    .pipe(map((res: any) => res.json())
+    ,catchError(this.errorHandler)
+    );  
+  }
+
+  errorHandler(error: HttpErrorResponse)
+  {
+      
+      return Observable.throwError("User not Found!!!")
   }
 
   updateProfile(username:string){
